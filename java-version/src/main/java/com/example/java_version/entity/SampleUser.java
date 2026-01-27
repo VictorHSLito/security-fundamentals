0a45a7f8d705cf95b6ac909863b68d637cdf6219
Victor <victorferrarihugo@hotmail.com>
2026-01-26 22:32:36 -0300

    feat: Make SampleUser implements UserDetails interface

--- java-version/src/main/java/com/example/java_version/entity/SampleUser.java
+++ java-version/src/main/java/com/example/java_version/entity/SampleUser.java
@@ -4,13 +4,20 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Builder
 @Table(name = "tb_user")
 @NoArgsConstructor
 @AllArgsConstructor
-public class SampleUser {
+public class SampleUser implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
@@ -23,4 +30,39 @@ public class SampleUser {
 
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
