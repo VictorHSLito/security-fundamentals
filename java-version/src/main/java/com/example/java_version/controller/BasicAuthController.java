9767d74a30449a384835317ee87c59b46598728f
Victor <victorferrarihugo@hotmail.com>
2026-01-26 22:33:48 -0300

    feat: Update Controller

--- java-version/src/main/java/com/example/java_version/controller/BasicAuthController.java
+++ java-version/src/main/java/com/example/java_version/controller/BasicAuthController.java
@@ -1,5 +1,6 @@
 package com.example.java_version.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BasicAuthController {
 
     @PostMapping("/login")
-    public void login() {}
+    public ResponseEntity<Void> login() {
+        return ResponseEntity.ok().build();
+    }
 }
